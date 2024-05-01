6e73aac6344e80545acddcf63fb973e454ec0150
Aramazd <apujanyan04@gmail.com>
2024-05-01 15:08:38 +0400

    custom MRO

---
+++ mro_task/costum_mro.py
@@ -0,0 +1,36 @@
+"""Custom python MRO implementation"""
+
+
+def merge(*mros):
+    result = []
+    while True:
+        not_empty_mros = [mro for mro in mros if mro]
+        if not not_empty_mros:
+            return result
+        for mro in not_empty_mros:
+            candidate = mro[0]
+            not_good_head = [h for h in not_empty_mros if candidate in h[1:]]
+            if not_good_head:
+                candidate = None
+            else:
+                break
+        if not candidate:
+            raise TypeError("Cannot create Method Resolution Order!")
+        result.append(candidate)
+        for m in not_empty_mros:
+            if m[0] == candidate:
+                del m[0]
+
+
+def build_mro(cls):
+    if cls == object:
+        return [object]
+    return [cls] + merge(*[build_mro(base) for base in cls.__bases__],
+                               list(cls.__bases__))
+
+
+def mro(cls):
+    return tuple(build_mro(cls))
+
+
+
