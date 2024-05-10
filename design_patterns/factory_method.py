2bb673142aeb688c9d976eca834eed2fa92ddf46
apujanyan <apujanyan04@gmail.com>
2024-05-10 16:12:46 +0400

    factory method

--- design_patterns/factory_method.py
+++ design_patterns/factory_method.py
@@ -1,66 +1,50 @@
-"""Factory Method design pattern example"""
+"""Factory method example"""
 
 from abc import ABC, abstractmethod
 
 
-class Package(ABC):
+class Animal(ABC):
     @abstractmethod
-    def run(self) -> None:
+    def speak(self) -> None:
         ...
 
 
-class DebianPackage(Package):
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.extension = '.deb'
-        self.full_name = name + self.extension
-
-    def run(self) -> None:
-        print(f'Running {self.full_name}...')
+class Dog(Animal):
+    def speak(self) -> None:
+        print('Dog speaks!')
 
 
-class RedHatPackage(Package):
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.extension = '.rpm'
-        self.full_name = name + self.extension
-
-    def run(self) -> None:
-        print(f'Running {self.full_name}...')
+class Cat(Animal):
+    def speak(self) -> None:
+        print('Cat speaks!')
 
 
-class PackageManager(ABC):
-    @staticmethod
+class Factory(ABC):
     @abstractmethod
-    def download_package(package_name: str) -> Package:
+    def create_animal(self) -> Animal:
         ...
 
 
-class APT(PackageManager):
-    @staticmethod
-    def download_package(package_name: str) -> DebianPackage:
-        if not package_name[-4::] == '.deb':
-            raise ValueError('The package extension must be .deb!')
-        print(f'The package {package_name} was successfully downloaded.')
-        return DebianPackage(package_name)
+class DogFactory(Factory):
+    def create_animal(self) -> Dog:
+        return Dog()
 
 
-class DNF(PackageManager):
-    @staticmethod
-    def download_package(package_name: str) -> RedHatPackage:
-        if not package_name[-4::] == '.rpm':
-            raise ValueError('The package extension must be .rpm!')
-        print(f'The package {package_name} was successfully downloaded.')
-        return RedHatPackage(package_name)
+class CatFactory(Factory):
+    def create_animal(self) -> Cat:
+        return Cat()
 
 
 def main() -> None:
-    deb_package = APT.download_package('deb_package.deb')
-    rpm_package = DNF.download_package('rpm_package.rpm')
-    deb_package.run()
-    rpm_package.run()
+    dog_factory = DogFactory()
+    cat_factory = CatFactory()
+
+    d1 = dog_factory.create_animal()
+    c1 = cat_factory.create_animal()
+
+    d1.speak()
+    c1.speak()
 
 
 if __name__ == '__main__':
     main()
-
