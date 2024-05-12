48064846275afcf7f7cc1d3614bb9870fd26b09e
apujanyan <apujanyan04@gmail.com>
2024-05-12 15:50:38 +0400

    property

--- customs/property.py
+++ customs/property.py
@@ -12,32 +12,32 @@ class Property:
         self.fget = fget
         self.fset = fset
         self.fdel = fdel
-        self.doc = doc
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
 
-    def __get__(self, instance, owner) -> None:
+    def __get__(self, instance, owner):
         if instance is None:
             return self
         if self.fget is None:
             raise AttributeError('Unreadable attribute!')
+        return self.fget(instance)
 
     def __set__(self, instance, value):
         if self.fset is None:
-            raise AttributeError('Cannot set the attribute!')
+            raise AttributeError('Unsettable attribute!')
         self.fset(instance, value)
 
     def __delete__(self, instance):
         if self.fdel is None:
-            raise AttributeError('Cannot delete the attribute!')
+            raise AttributeError('Undeletable attribute!')
         self.fdel(instance)
 
     def getter(self, fget):
-        return type(self)(fget, self.fset, self.fdel, self.doc)
+        return type(self)(fget, self.fset, self.fdel, self.__doc__)
 
     def setter(self, fset):
-        return type(self)(self.fget, fset, self.fdel, self.doc)
+        return type(self)(self.fget, fset, self.fdel, self.__doc__)
 
     def deleter(self, fdel):
-        return type(self)(self.fget, self.fset, fdel, self.doc)
-
-
-
+        return type(self)(self.fget, self.fset, fdel, self.__doc__)
