e0f703678dc7b9349156391cda11d5953d6e0fde
apujanyan <apujanyan04@gmail.com>
2024-05-12 16:11:11 +0400

    typechecker

--- customs/typechecker.py
+++ customs/typechecker.py
@@ -1,6 +1,9 @@
-"""Typechecker decorator for class __init__ type checking"""
+"""Typechecker decorator for class __init__ type checking. """
 
-def typechecker(cls):
+
+def typecheck(cls):
+    """Type checker decorator as function. """
+
     def wrapper(*args, **kwargs):
         init = cls.__init__
         types = init.__annotations__
@@ -10,19 +13,43 @@ def typechecker(cls):
         for arg_value, arg_name in zip(args, var_names):
             if arg_name in types:
                 if not isinstance(arg_value, types[arg_name]):
-                    raise TypeError(f"Expected {types[arg_name]}, "
-                                    f"got {type(arg_value)}!")
+                    raise TypeError(f"Argument {arg_name} expected "
+                                    f"{types[arg_name].__name__}")
 
         # For keyword arguments
         for arg_name, arg_value in kwargs.items():
             if arg_name in types:
                 if not isinstance(arg_value, types[arg_name]):
-                    raise TypeError(f"Expected {types[arg_name]}, "
-                                    f"got {type(arg_value)}!")
+                    raise TypeError(f"Argument {arg_name} expected "
+                                    f"{types[arg_name].__name__}")
+
         return cls(*args, **kwargs)
     return wrapper
 
 
+class Typecheck:
+    """Type checker decorator as class. """
 
+    def __init__(self, cls):
+        self.cls = cls
 
+    def __call__(self, *args, **kwargs):
+        init = self.cls.__init__
+        types = init.__annotations__
+        varnames = init.__code__.co_varnames[1::]
 
+        # For positional arguments
+        for arg_name, arg_value in zip(varnames, args):
+            if arg_name in types:
+                if not isinstance(arg_value, types[arg_name]):
+                    raise TypeError(f"Argument {arg_name} expected "
+                                    f"{types[arg_name].__name__}")
+
+        # For keyword arguments
+        for arg_name, arg_value in kwargs.items():
+            if arg_name in types:
+                if not isinstance(arg_value, types[arg_name]):
+                    raise TypeError(f"Argument {arg_name} expected "
+                                    f"{types[arg_name].__name__}")
+
+        return self.cls(*args, **kwargs)
